0a4eb31ebb236290e7e29cee05e70429cbbc8edc
snidarian <cephalopod31956@gmail.com>
2021-06-16 17:18:58 -0400

    color set hex list added

--- hex_color_sets/color_sets.py
+++ hex_color_sets/color_sets.py
@@ -5,4 +5,9 @@ kirishima_eijirou = ['#292a2c', '#6a1611', '#bc342d', '#85201b', '#bababb']
 
 HHteal = ['#47b3a3', '#1dbaa4', '#00bfb0', '#00bfa7', '#2ec3a1']
 
+sero_hanta = ['#ffeb8c', '#fcbe6c', '#e58a39', '#73451d', '#000000']
+
+bakugou_katsuki_mha = ['#27483d', '#438844', '#d04931', '#faa722', '#efdcbb']
+
+chill_blues_super_light = ['#b7f2ff', '#b7e2ff', '#b7d2ff', '#b7c2ff', '#b7b2ff']
 
